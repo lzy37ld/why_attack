23cb2f23522cb199516ec775c4d51d3a14037344
lzy37ld <lzy37ld@gmail.com>
2023-12-01 16:18:06 -0500

    1

--- run_cli.sh
+++ run_cli.sh
@@ -3,20 +3,20 @@ set -x
 set -e
 
 # prompway = no
-python evaluate.py prompt_way=no append_label_length=-1
-python evaluate.py prompt_way=no append_label_length=3
-python evaluate.py prompt_way=no append_label_length=6
+# python evaluate.py prompt_way=no append_label_length=-1
+# python evaluate.py prompt_way=no append_label_length=3
+# python evaluate.py prompt_way=no append_label_length=6
 
-python evaluate.py prompt_way=no append_label_length=-1 target_lm.generation_configs.do_sample=True
-python evaluate.py prompt_way=no append_label_length=3 target_lm.generation_configs.do_sample=True
-python evaluate.py prompt_way=no append_label_length=6 target_lm.generation_configs.do_sample=True
+# python evaluate.py prompt_way=no append_label_length=-1 target_lm.generation_configs.do_sample=True
+# python evaluate.py prompt_way=no append_label_length=3 target_lm.generation_configs.do_sample=True
+# python evaluate.py prompt_way=no append_label_length=6 target_lm.generation_configs.do_sample=True
 
 
 # promptway = own
-python evaluate.py prompt_own=own append_label_length=-1
-python evaluate.py prompt_own=own append_label_length=3
-python evaluate.py prompt_own=own append_label_length=6
+python evaluate.py prompt_way=own append_label_length=-1
+python evaluate.py prompt_way=own append_label_length=3
+python evaluate.py prompt_way=own append_label_length=6
 
-python evaluate.py prompt_own=own append_label_length=-1 target_lm.generation_configs.do_sample=True
-python evaluate.py prompt_own=own append_label_length=3 target_lm.generation_configs.do_sample=True
-python evaluate.py prompt_own=own append_label_length=6 target_lm.generation_configs.do_sample=True
+python evaluate.py prompt_way=own append_label_length=-1 target_lm.generation_configs.do_sample=True
+python evaluate.py prompt_way=own append_label_length=3 target_lm.generation_configs.do_sample=True
+python evaluate.py prompt_way=own append_label_length=6 target_lm.generation_configs.do_sample=True
