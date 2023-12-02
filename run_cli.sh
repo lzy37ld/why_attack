ac4bdffba5aad91706def8e7ac2e8176df691052
lzy37ld <lzy37ld@gmail.com>
2023-12-02 00:55:09 -0500

    1

--- run_cli.sh
+++ run_cli.sh
@@ -2,14 +2,14 @@
 set -x 
 set -e
 
-# prompway = no
-# python evaluate.py prompt_way=no append_label_length=-1
-# python evaluate.py prompt_way=no append_label_length=3
-# python evaluate.py prompt_way=no append_label_length=6
+prompway = no
+python evaluate.py prompt_way=no append_label_length=-1
+python evaluate.py prompt_way=no append_label_length=3
+python evaluate.py prompt_way=no append_label_length=6
 
-# python evaluate.py prompt_way=no append_label_length=-1 target_lm.generation_configs.do_sample=True
-# python evaluate.py prompt_way=no append_label_length=3 target_lm.generation_configs.do_sample=True
-# python evaluate.py prompt_way=no append_label_length=6 target_lm.generation_configs.do_sample=True
+python evaluate.py prompt_way=no append_label_length=-1 target_lm.generation_configs.do_sample=True
+python evaluate.py prompt_way=no append_label_length=3 target_lm.generation_configs.do_sample=True
+python evaluate.py prompt_way=no append_label_length=6 target_lm.generation_configs.do_sample=True
 
 
 # promptway = own
@@ -17,9 +17,9 @@ python evaluate.py prompt_way=own append_label_length=-1
 python evaluate.py prompt_way=own append_label_length=3
 python evaluate.py prompt_way=own append_label_length=6
 
-python evaluate.py prompt_own=own append_label_length=-1 target_lm.generation_configs.do_sample=True
-python evaluate.py prompt_own=own append_label_length=3 target_lm.generation_configs.do_sample=True
-python evaluate.py prompt_own=own append_label_length=6 target_lm.generation_configs.do_sample=True
+python evaluate.py prompt_way=own append_label_length=-1 target_lm.generation_configs.do_sample=True
+python evaluate.py prompt_way=own append_label_length=3 target_lm.generation_configs.do_sample=True
+python evaluate.py prompt_way=own append_label_length=6 target_lm.generation_configs.do_sample=True
 
 
 
@@ -36,13 +36,13 @@ python evaluate.py prompt_way=no append_label_length=6 target_lm.generation_conf
 
 
 # promptway = own
-python evaluate.py prompt_own=own append_label_length=-1 target_lm.generation_configs.max_new_tokens=100
-python evaluate.py prompt_own=own append_label_length=3 target_lm.generation_configs.max_new_tokens=100
-python evaluate.py prompt_own=own append_label_length=6 target_lm.generation_configs.max_new_tokens=100
+python evaluate.py prompt_way=own append_label_length=-1 target_lm.generation_configs.max_new_tokens=100
+python evaluate.py prompt_way=own append_label_length=3 target_lm.generation_configs.max_new_tokens=100
+python evaluate.py prompt_way=own append_label_length=6 target_lm.generation_configs.max_new_tokens=100
 
-python evaluate.py prompt_own=own append_label_length=-1 target_lm.generation_configs.do_sample=True target_lm.generation_configs.max_new_tokens=100
-python evaluate.py prompt_own=own append_label_length=3 target_lm.generation_configs.do_sample=True target_lm.generation_configs.max_new_tokens=100
-python evaluate.py prompt_own=own append_label_length=6 target_lm.generation_configs.do_sample=True target_lm.generation_configs.max_new_tokens=100
+python evaluate.py prompt_way=own append_label_length=-1 target_lm.generation_configs.do_sample=True target_lm.generation_configs.max_new_tokens=100
+python evaluate.py prompt_way=own append_label_length=3 target_lm.generation_configs.do_sample=True target_lm.generation_configs.max_new_tokens=100
+python evaluate.py prompt_way=own append_label_length=6 target_lm.generation_configs.do_sample=True target_lm.generation_configs.max_new_tokens=100
 
 
 
